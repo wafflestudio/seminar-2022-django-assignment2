940d17004ff376c6b573ac728e52ea40dcb768fd
BDlhj <akk808@naver.com>
2022-11-05 17:54:24 +0900

    [feat] Add 'IsAuthor' permission

--- blogProject/posts/permissions.py
+++ blogProject/posts/permissions.py
@@ -1,8 +1,14 @@
 from rest_framework import permissions
+from rest_framework.permissions import IsAdminUser
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+        return obj.author == request.user
+
+
+class IsAuthor(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
         return obj.author == request.user
