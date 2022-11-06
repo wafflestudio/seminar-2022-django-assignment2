ca8b96047ef17d19bf7f0ecc0890ce4fa35b51fa
Jinan <jinanjeong@snu.ac.kr>
2022-11-06 16:56:38 +0900

    implement token authentication

--- mediumAlternative/mediumAlternative/urls.py
+++ mediumAlternative/mediumAlternative/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', views.obtain_auth_token)
 ]
