2f277fe789ecb55e38742575541bb72af85ed113
serverrepairman <gustjwkd1007@gmail.com>
2022-11-08 04:43:34 +0900

    update DATABASE settings

--- ServerrepairmanBlog/ServerrepairmanBlog/settings.py
+++ ServerrepairmanBlog/ServerrepairmanBlog/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'ServerrepairmanBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'myusername',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
